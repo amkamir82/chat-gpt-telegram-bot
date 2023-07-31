cd12009a0fcc4d8de4e74d1375aa02721968ed71
ce424-012-98171053 <amk_amir82@yahoo.com>
2023-07-31 15:33:49 +0330

    add config file

---
+++ code/config.py
@@ -0,0 +1,5 @@
+OPEN_AI_API_KEY = "xxxxx"  # You can get this API Key from openai.com
+API_ID = 99999  # You can get App ID from my.telegram.org
+API_HASH = "xxxxx"  # You can get API hash from my.telegram.org
+BOT_TOKEN = "xxxxx"  # You can get telegram bot API token from @BotFather
+BLOCK_LIST_CHATS = ["xxxxx"]  # You can enter the chat ID of those you don't want the bot to respond to in this list
